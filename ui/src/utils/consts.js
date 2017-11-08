7804ba4d4dcdd5f1fbe4fa6aacafd42621b6859d
dmitryk-dk <kozlovdmitriyy@gmail.com>
2017-11-09 00:03:28 +0200

    fix endpoints

--- ui/src/utils/consts.js
+++ ui/src/utils/consts.js
@@ -3,6 +3,7 @@ export const START = 'api/v1/dialer/start';
 export const STOP = 'api/v1/dialer/stop';
 export const APP_STATUS = 'api/v1/dialer/status';
 
+
 const ACTION = 'action';
 const EXTEN = 'exten';
 const PEER = 'peer';
