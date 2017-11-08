54256ed2f74eb0ebc71295a5a84b808dab4f2083
Vyacheslav Pryimak <vyacheslavpryimak@gmail.com>
2017-11-08 21:17:16 +0200

    js fixes

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App';
-import css from './styles/style.scss';
-import * as actions from './actions/actions'
 import * as consts from './utils/consts';
 
-const getData = () =
->
-{
+const getData = () => {
     const registeredUsersUrl = consts.getHostFn().replace('{API}', consts.registeredUSers);
     const dialerStatusUrl = consts.getHostFn().replace('{API}', consts.status);
     //actions.getRegisteredUsers(registeredUsersUrl);
     //actions.getDialerStatus(dialerStatusUrl);
-}
-;
+};
 
 function init() {
     getData();
-    render( < App / >, document.getElementById('app')
-)
-    ;
+    render(<App/>, document.getElementById('app'));
 }
 
 init();
