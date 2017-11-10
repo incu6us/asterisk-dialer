350b28d4e521781dddf69a1462d9a5edf29c0262
dmitryk-dk <kozlovdmitriyy@gmail.com>
2017-11-10 12:40:49 +0200

    fix updating

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -22,13 +22,16 @@ class App extends Component {
         }
     }
 
+    componentWillUpdate(newProps, newState) {
+        console.log(newState);
+        if (newState.dialerLists.length === 0) {
+            actions.getCallInProgress(CONSTS.API[CONSTS.CALL_IN_PROGRESS]);
+        }
+    }
+
     componentDidMount(){
         setInterval(()=> actions.getRegisteredUsers(CONSTS.API[CONSTS.REGISTERED_USERS]), 10000);
         setInterval(()=> actions.getDialerStatus(CONSTS.API[CONSTS.APP_STATUS]), 1000);
-
-        if (this.state.dialerLists.length === 0) {
-            actions.getCallInProgress(CONSTS.API[CONSTS.CALL_IN_PROGRESS]);
-        }
     }
 
     render() {
