8bc9a5a06b51359cf2ac50deab93c1e3ee302470
Vyacheslav Pryimak <vyacheslavpryimak@gmail.com>
2017-11-12 14:17:45 +0200

    count records fix

--- platform/database/query.go
+++ platform/database/query.go
@@ -6,6 +6,7 @@ const (
     MsisdnAll                      = iota
     MsisdnInProgress
     MsisdnInProgressWithPagination
+    MsisdnInProgressCount
 )
 
 const (
@@ -13,7 +14,8 @@ const (
 )
 
 func GetQuery(key int) string {
-    queries[MsisdnInProgress] = `SELECT l.*, p.* FROM msisdn_lists l, msisdn_priorities p WHERE l.id = p.msisdn_id AND (` + InProgressSuffix + `) ORDER BY %s %s`
-    queries[MsisdnInProgressWithPagination] = `SELECT l.*, p.* FROM msisdn_lists l, msisdn_priorities p WHERE l.id = p.msisdn_id AND (` + InProgressSuffix + `) ORDER BY %s %s LIMIT %d OFFSET %d`
+    queries[MsisdnInProgressCount] = `SELECT count(l.*) FROM msisdn_lists l, msisdn_priorities p WHERE l.id = p.msisdn_id AND (` + InProgressSuffix + `)`
+    queries[MsisdnInProgress] = `SELECT l.*, p.* FROM msisdn_lists l, msisdn_priorities p WHERE l.id = p.msisdn_id AND (` + InProgressSuffix + `) ORDER BY %s %s, l.id`
+    queries[MsisdnInProgressWithPagination] = `SELECT l.*, p.* FROM msisdn_lists l, msisdn_priorities p WHERE l.id = p.msisdn_id AND (` + InProgressSuffix + `) ORDER BY %s %s, l.id LIMIT %d OFFSET %d`
     return queries[key]
 }
